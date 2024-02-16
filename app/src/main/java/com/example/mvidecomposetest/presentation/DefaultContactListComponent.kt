a4a9e3ac4890160228fd33483c6fa5cfef031e81
Artem <artem.mamedov5375@gmail.com>
2024-02-16 18:05:14 +0200

    Implement ComponentContext

---
+++ app/src/main/java/com/example/mvidecomposetest/presentation/DefaultContactListComponent.kt
@@ -0,0 +1,44 @@
+package com.example.mvidecomposetest.presentation
+
+import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.essenty.backhandler.BackHandler
+import com.arkivanov.essenty.instancekeeper.InstanceKeeper
+import com.arkivanov.essenty.lifecycle.Lifecycle
+import com.arkivanov.essenty.statekeeper.StateKeeper
+import com.example.mvidecomposetest.core.componentScope
+import com.example.mvidecomposetest.data.RepositoryImpl
+import com.example.mvidecomposetest.domain.Contact
+import com.example.mvidecomposetest.domain.GetContactsUseCase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class DefaultContactListComponent(
+    componentContext: ComponentContext,
+    val onEditingContactRequest: (Contact) -> Unit,
+    val onAddContactRequest: () -> Unit
+) : ContactListComponent, ComponentContext by componentContext {
+
+    private val repository = RepositoryImpl
+    private val getContactsUseCase = GetContactsUseCase(repository)
+    private val coroutineScope = componentScope()
+
+    override val model: StateFlow<ContactListComponent.Model> = getContactsUseCase()
+        .map { ContactListComponent.Model(it) }
+        .stateIn(
+            scope = coroutineScope,
+            started = SharingStarted.Lazily,
+            initialValue = ContactListComponent.Model(listOf())
+        )
+
+    override fun onContactClicked(contact: Contact) {
+        onEditingContactRequest(contact)
+    }
+
+    override fun onAddContactClicked() {
+        onAddContactRequest()
+    }
+}
